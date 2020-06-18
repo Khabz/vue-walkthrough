c3be69255560928f94ffad10be15e31a4574e752
khabubundivhu <khabubundivh@gmail.com>
2020-06-18 05:28:31 +0200

    update packages

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-# vue-walkthrough
+## Vue Tutorial
+### We're going to create a small application with Vue.
+
+[Read the tutorial](khabubundivhu.co.za/getting-started-with-vue)
+Purpose
+The app will be a simple employee database and we'll learn:
+
+### How to set up Vue
+- The anatomy of a Vue file
+- How to work with data, methods, conditional statements, and events in Vue
+- How to create, update, view, and delete users (employees) from the system
+- How to make API calls for each of the above actions
+- How to use tables, forms, and form validation
+- How to host a Vue build on GitHub pages
 
 ## Project setup
 ```
--- src/components/EmployeeForm.vue
+++ src/components/EmployeeForm.vue
@@ -5,6 +5,9 @@
         <label>First Name</label>
         <input
           v-model="employee.firstName"
+          :class="{ 'has-error': submitting && invalidFirstName }"
+          @focus="clearStatus"
+          @keypress="clearStatus"
           type="text"
           class="form-control"
           placeholder="Employee's First Name"
@@ -14,6 +17,9 @@
         <label>Last Name</label>
         <input
           v-model="employee.lastName"
+          :class="{ 'has-error': submitting && invalidLastName }"
+          @focus="clearStatus"
+          @keypress="clearStatus"
           type="text"
           class="form-control"
           placeholder="Employee's Last Name"
@@ -23,10 +29,14 @@
         <label>Email</label>
         <input
           v-model="employee.email"
+          :class="{ 'has-error': submitting && invalidEmail }"
+          @focus="clearStatus"
           type="email"
           class="form-control"
           placeholder="Employee's Email Address"
         />
+        <p v-if="error && submitting" class="error-message">❗ Please fill out all required fields</p>
+        <p v-if="success" class="success-message">✅ Employee successfully added</p>
       </div>
       <button class="btn btn-primary">Submit</button>
     </form>
@@ -38,6 +48,9 @@ export default {
   name: "EmployeeForm",
   data() {
     return {
+      submitting: false,
+      error: false,
+      success: false,
       employee: {
         firstName: "",
         lastName: "",
@@ -47,8 +60,58 @@ export default {
   },
   methods: {
     handleSubmit() {
+      this.submitting = true;
+      this.clearStatus();
+
+      if (this.invalidFirstName || this.invalidLastName || this.invalidEmail) {
+        this.error = true;
+        return;
+      }
+
       this.$emit("add:employee", this.employee);
+      this.employee = {
+        firstName: "",
+        lastName: "",
+        email: ""
+      };
+      this.error = false;
+      this.success = true;
+      this.submitting = false;
+    },
+
+    clearStatus() {
+      this.success = false;
+      this.error = false;
+    }
+  },
+  computed: {
+    invalidFirstName() {
+      return this.employee.firstName == '';
+    },
+    invalidLastName() {
+      return this.employee.lastName == '';
+    },
+    invalidEmail() {
+      return this.employee.email == '';
     }
   }
 };
 </script>
+
+<style scoped>
+form {
+  margin-bottom: 2rem;
+}
+
+.form-control {
+  font-weight: 500;
+}
+
+.error-message {
+  color: #d33c40;
+}
+
+.success-message {
+  color: #32a95d;
+}
+</style>
